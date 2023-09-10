686936ede20b6ca73fed3252839ec88e54cf4260
pt1997 <pt1997@xyz.de>
2023-09-10 18:02:12 +0200

    hyprland

---
+++ .config/lockscreen/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+systemctl --user is-active --quiet lisgd
+if [ $? -eq 0 ]
+then
+	systemctl --user stop lisgd
+	imv -fs crop wallpaper.jpg &
+	hyprctl dispatch dpms off
+else
+	hyprctl dispatch dpms on
+	lisgd-unlock/lisgd &
+	PID=$!
+	sleep 15
+	systemctl --user is-active --quiet lisgd
+	if [ $? -ne 0 ]
+	then
+		hyprctl dispatch dpms off
+	fi
+
+	kill $PID
+	# todos:
+	# show nwg-wrapper
+fi
+
